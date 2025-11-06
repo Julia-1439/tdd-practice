9f070618910e5b2be53e5f20e53d968709ef5443
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-06 11:47:56 -0800

    Test calc.add: all tests passed

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,8 @@
 const calculator = (() => {
   function add(a, b) {
-    if (a === 4 && b === 5)
-      return 9;
-    else
-      return 2;
+    if (typeof a !== "number" || typeof b !== "number")
+      throw new Error("Inputs must be a number only");
+    return a + b;
   }
 
   return {
--- src/calculator.spec.js
+++ src/calculator.spec.js
@@ -5,10 +5,21 @@ test("calculator exists", () => {
 });
 
 describe("add", () => {
-  test("happy case (1): positive nums", () => expect(calc.add(4, 5)).toBe(9));
-  test("happy case (2): one negative num", () => expect(calc.add(-2, 4)).toBe(2));
+  test("happy case (1): two positive nums", () =>
+    expect(calc.add(4, 5)).toBe(9));
+  test("happy case (2): one negative num", () =>
+    expect(calc.add(-2, 4)).toBe(2));
+  test("happy case (3): two negative nums", () =>
+    expect(calc.add(-10, -100)).toBe(-110));
+  test("happy case (4): adding zero", () => 
+    expect(calc.add(0, 5)).toBe(5));
+  test("happy case (5): adding two zeros", () => 
+    expect(calc.add(0, 0)).toBe(0));
+  test("edge case (1): one invalid type", () => 
+    expect(() => calc.add("2", 4)).toThrow());
+  test("edge case (2): two invalid types", () => 
+    expect(() => calc.add("5", "10")).toThrow());
 });
 
-
-// happy path for each operation: positive & negative numbers
-// edge: invalid variable type, zero
+// happy path for each operation: positive & negative numbers & zero
+// edge: invalid variable type
