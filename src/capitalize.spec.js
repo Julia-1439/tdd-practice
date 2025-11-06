ef82da8fd95972c41511d63f109b3c697ba80c14
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-06 10:22:27 -0800

    Test capitalize: edge case of empty string

--- src/capitalize.spec.js
+++ src/capitalize.spec.js
@@ -1,9 +1,13 @@
 import { capitalize } from "./capitalize.js";
 
-test('happy path (1)', () => {
+test("happy path (1)", () => {
   expect(capitalize("hello world")).toBe("Hello world");
 });
 
-test('happy path (2)', () => {
+test("happy path (2)", () => {
   expect(capitalize("per my previous email")).toBe("Per my previous email");
+});
+
+test("does nothing to empty string", () => {
+  expect(capitalize("")).toBe("");
 });
