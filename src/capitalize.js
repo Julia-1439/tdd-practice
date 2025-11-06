607f3c0a1b10a1b6150c03c4ad697a419ce40d10
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-11-06 10:23:53 -0800

    Refactor: use default import over named import

--- src/capitalize.js
+++ src/capitalize.js
@@ -3,4 +3,4 @@ function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
-export { capitalize };
+export default capitalize;
--- src/capitalize.spec.js
+++ src/capitalize.spec.js
@@ -1,4 +1,4 @@
-import { capitalize } from "./capitalize.js";
+import capitalize from "./capitalize.js";
 
 test("happy path (1)", () => {
   expect(capitalize("hello world")).toBe("Hello world");
